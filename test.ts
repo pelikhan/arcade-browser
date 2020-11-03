f7ed4064e16224bd6207e32f44406f369a47a3d7
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-11-03 12:14:01 -0800

    Update pxt.json, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,3 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+controller.A.onEvent(ControllerButtonEvent.Pressed, function() {
+    web.open("https://makecode.com/")
+})
