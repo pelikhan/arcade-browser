6b3c04fff28d1e73a1efe1ae3e881c41dc128e3d
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-11-03 13:33:28 -0800

    simplify example

--- test.ts
+++ test.ts
@@ -1,4 +1,2 @@
 game.splash("press A to browse to makecode.com")
-controller.A.onEvent(ControllerButtonEvent.Pressed, function() {
-    web.open("https://makecode.com/")
-})
+web.open("https://makecode.com/")
